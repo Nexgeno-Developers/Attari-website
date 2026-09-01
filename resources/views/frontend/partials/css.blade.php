6921389f06120479b51a4247b312e4915d28cc87
Nexgeno-Developers <webdeveloper@nexgeno.in>
2026-09-01 16:28:27 +0530

    Update course certificates styles for improved layout and visibility

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -1,6 +1,6 @@
 
 <link rel="shortcut icon" href="{{ asset('/assets/frontend/images/favicon.webp') }}">
-<link rel="stylesheet" href="/assets/frontend/css/style.css?v1.9.12" />
+<link rel="stylesheet" href="/assets/frontend/css/style.css?v1.9.13" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
  
 <link rel="stylesheet" href="/assets/frontend/css/responsive.css?v-1.3.9" />
