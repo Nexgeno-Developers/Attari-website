6d470d031eb7c819b001776bef781ecd61c36a8c
Nexgeno-Developers <webdeveloper@nexgeno.in>
2026-09-04 17:01:48 +0530

    Update custom.js version for improved functionality

--- resources/views/frontend/partials/js.blade.php
+++ resources/views/frontend/partials/js.blade.php
@@ -71,6 +71,6 @@
 
 <!--Custom Js-->
 <script src="/assets/frontend/js/Init.js?1.1.0"></script>
-<script src="/assets/frontend/js/custom.js?1.2.7"></script>
+<script src="/assets/frontend/js/custom.js?1.2.91"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js"></script>
 
